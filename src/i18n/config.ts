c40d1ef146ca50d59ede1e02afed0a93a72fa655
Leonardo Lopes <leonardolopesdeveloper@gmail.com>
2026-03-10 13:31:13 -0300

    chore: add english translations

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,17 +1,23 @@
 import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
+import en from './languages/en.json';
 import ptBR from './languages/pt-BR.json';
 
-i18n.use(initReactI18next).init({
-	resources: {
-		'pt-BR': ptBR,
-	},
-	fallbackLng: 'pt-BR',
-	lng: 'pt-BR',
-	interpolation: {
-		escapeValue: false,
-	},
-});
+i18n
+	.use(LanguageDetector)
+	.use(initReactI18next)
+	.init({
+		resources: {
+			en,
+			'pt-BR': ptBR,
+		},
+		fallbackLng: 'en',
+		supportedLngs: ['en', 'pt-BR'],
+		interpolation: {
+			escapeValue: false,
+		},
+	});
 
 export const t = i18n.t.bind(i18n);
--- src/screens/dmc1/index.tsx
+++ src/screens/dmc1/index.tsx
@@ -42,7 +42,7 @@ export default function Dmc1Screen() {
 
 	useBeforeUnload(isValid);
 
-	if (!isValid) return <Navigate to="/" replace />;
+	if (!isValid) return <Navigate to="/" />;
 
 	return (
 		<main className="flex-1 flex flex-col px-4 py-8 gap-6 max-w-3xl mx-auto w-full">
